1e7fd935494ad7bfa9a37f481e8b51a267bdf805
ybkim-sayouzone <ybkim@sayouzone.com>
2025-12-19 16:39:44 +0900

    feat: Add __init__.py to enable module imports in Assembler

---
+++ packages/sayou-assembler/src/sayou/assembler/__init__.py
@@ -0,0 +1,11 @@
+from .pipelines import AssemblerPipeline
+from .builder.graph_builder import GraphBuilder
+from .builder.vector_builder import VectorBuilder
+from .plugins.cypher_builder import CypherBuilder
+
+__all__ = [
+    "AssemblerPipeline",
+    "GraphBuilder",
+    "VectorBuilder",
+    "CypherBuilder",
+]
