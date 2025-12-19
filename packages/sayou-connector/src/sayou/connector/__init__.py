bbed9fb4b909afc0015ed281858753e3ffbc8a7e
ybkim-sayouzone <ybkim@sayouzone.com>
2025-12-19 16:30:04 +0900

    feat: Add __init__.py to enable module imports in Connector

---
+++ packages/sayou-connector/src/sayou/connector/__init__.py
@@ -0,0 +1,11 @@
+from .pipelines import ConnectorPipeline
+from .generator.file_generator import FileGenerator
+from .generator.requests_generator import RequestsGenerator
+from .generator.sqlite_generator import SqliteGenerator
+
+__all__ = [
+    "ConnectorPipeline",
+    "FileGenerator",
+    "RequestsGenerator",
+    "SqliteGenerator",
+]
