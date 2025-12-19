41e2391364c289c631338fa0d7b62c970af12f5e
ybkim-sayouzone <ybkim@sayouzone.com>
2025-12-19 16:40:33 +0900

    feat: Add __init__.py to enable module imports in Loader

---
+++ packages/sayou-loader/src/sayou/loader/__init__.py
@@ -0,0 +1,13 @@
+from .pipelines import LoaderPipeline
+from .writer.console_writer import ConsoleWriter
+from .writer.file_writer import FileWriter
+from .writer.jsonl_writer import JsonLineWriter
+from .plugins.neo4j_writer import Neo4jWriter
+
+__all__ = [
+    "LoaderPipeline",
+    "ConsoleWriter",
+    "FileWriter",
+    "JsonLineWriter",
+    "Neo4jWriter",
+]
