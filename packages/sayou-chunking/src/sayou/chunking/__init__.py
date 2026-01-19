d0c2e540b4ab84196bf0f5fa18c7c1746f84f4bb
ybkim-sayouzone <ybkim@sayouzone.com>
2026-01-19 16:51:23 +0900

    style: Adhere to black style guide in Chunking

--- packages/sayou-chunking/src/sayou/chunking/__init__.py
+++ packages/sayou-chunking/src/sayou/chunking/__init__.py
@@ -1,13 +1,13 @@
 from .pipeline import ChunkingPipeline
+from .plugins.audited_fixed_length_splitter import AuditedFixedLengthSplitter
+from .plugins.langchain_splitter import LangchainSplitter
+from .plugins.markdown_splitter import MarkdownSplitter
 from .splitter.agentic_splitter import AgenticSplitter
 from .splitter.fixed_length_splitter import FixedLengthSplitter
 from .splitter.parent_document_splitter import ParentDocumentSplitter
 from .splitter.recursive_splitter import RecursiveSplitter
 from .splitter.semantic_splitter import SemanticSplitter
 from .splitter.structure_splitter import StructureSplitter
-from .plugins.audited_fixed_length_splitter import AuditedFixedLengthSplitter
-from .plugins.langchain_splitter import LangchainSplitter
-from .plugins.markdown_splitter import MarkdownSplitter
 
 __all__ = [
     "ChunkingPipeline",
