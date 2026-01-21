c9b31ac6cc136eba1dea9fd6384717d73a7843e0
ybkim-sayouzone <ybkim@sayouzone.com>
2026-01-21 10:04:38 +0900

    feat: Update __init__ for Refinery

--- packages/sayou-refinery/src/sayou/refinery/__init__.py
+++ packages/sayou-refinery/src/sayou/refinery/__init__.py
@@ -2,6 +2,8 @@ from .normalizer.doc_markdown_normalizer import DocMarkdownNormalizer
 from .normalizer.html_text_normalizer import HtmlTextNormalizer
 from .normalizer.record_normalizer import RecordNormalizer
 from .pipeline import RefineryPipeline
+from .plugins.link_processor import LinkProcessor
+from .plugins.white_space_processor import WhiteSpaceProcessor
 from .processor.deduplicator import Deduplicator
 from .processor.imputer import Imputer
 from .processor.outlier_handler import OutlierHandler
@@ -13,6 +15,8 @@ __all__ = [
     "DocMarkdownNormalizer",
     "HtmlTextNormalizer",
     "RecordNormalizer",
+    "LinkProcessor",
+    "WhiteSpaceProcessor",
     "Deduplicator",
     "Imputer",
     "OutlierHandler",
