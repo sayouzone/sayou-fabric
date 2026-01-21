82df2342a0619a3e65fe2cac129b03d78eafa95c
ybkim-sayouzone <ybkim@sayouzone.com>
2026-01-21 11:07:41 +0900

    feat: Update __init__ for Wrapper

--- packages/sayou-wrapper/src/sayou/wrapper/__init__.py
+++ packages/sayou-wrapper/src/sayou/wrapper/__init__.py
@@ -1,7 +1,11 @@
-from .pipeline import WrapperPipeline
 from .adapter.document_chunk_adapter import DocumentChunkAdapter
+from .pipeline import WrapperPipeline
+from .plugins.embedding_adapter import EmbeddingAdapter
+from .plugins.metadata_adapter import MetadataAdapter
 
 __all__ = [
     "WrapperPipeline",
     "DocumentChunkAdapter",
+    "EmbeddingAdapter",
+    "MetadataAdapter",
 ]
