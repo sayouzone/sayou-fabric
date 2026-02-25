32e4d0a9d54b83c455d93f0cc1e2da5a8e8c1dc5
ybkim-sayouzone <ybkim@sayouzone.com>
2026-02-25 11:02:10 +0900

    fix: [Brain] Modify the Brain init file

--- packages/sayou-brain/src/sayou/brain/__init__.py
+++ packages/sayou-brain/src/sayou/brain/__init__.py
@@ -1,10 +1,10 @@
-from .pipelines.normal import NormalPipeline
+# from .pipelines.normal import NormalPipeline
 from .pipelines.standard import StandardPipeline
 from .pipelines.structure import StructurePipeline
 from .pipelines.transfer import TransferPipeline
 
 __all__ = [
-    "NormalPipeline",
+    # "NormalPipeline",
     "StandardPipeline",
     "StructurePipeline",
     "TransferPipeline",
