a8e098e5375c56a382c0363ce5968a038cd5359d
ybkim-sayouzone <ybkim@sayouzone.com>
2026-02-25 11:01:00 +0900

    fix: [Loader] Modify the Loader init file

--- packages/sayou-loader/src/sayou/loader/__init__.py
+++ packages/sayou-loader/src/sayou/loader/__init__.py
@@ -1,24 +1,3 @@
 from .pipeline import LoaderPipeline
-from .plugins.chroma_writer import ChromaWriter
-from .plugins.elasticsearch_writer import ElasticsearchWriter
-from .plugins.mongodb_writer import MongoDBWriter
-from .plugins.neo4j_writer import Neo4jWriter
-from .plugins.postgres_writer import PostgresWriter
-from .writer.console_writer import ConsoleWriter
-from .writer.file_writer import FileWriter
-from .writer.jsonl_writer import JsonLineWriter
 
-# from .plugins.bigquery_writer import BigQueryWriter
-
-__all__ = [
-    "LoaderPipeline",
-    "ChromaWriter",
-    "ElasticsearchWriter",
-    "MongoDBWriter",
-    "Neo4jWriter",
-    "PostgresWriter",
-    "ConsoleWriter",
-    "FileWriter",
-    "JsonLineWriter",
-    # "BigQueryWriter"
-]
+__all__ = ["LoaderPipeline"]
