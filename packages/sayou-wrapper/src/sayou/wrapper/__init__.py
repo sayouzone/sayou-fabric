cc1e20b13a398caf6bcb41de55e48a1462594fec
ybkim-sayouzone <ybkim@sayouzone.com>
2026-02-26 10:34:40 +0900

    fix: [Wrapper] Modify the Wrapper init file

--- packages/sayou-wrapper/src/sayou/wrapper/__init__.py
+++ packages/sayou-wrapper/src/sayou/wrapper/__init__.py
@@ -1,15 +1,3 @@
-from .adapter.code_chunk_adapter import CodeChunkAdapter
-from .adapter.document_chunk_adapter import DocumentChunkAdapter
-from .adapter.video_chunk_adapter import VideoChunkAdapter
 from .pipeline import WrapperPipeline
-from .plugins.embedding_adapter import EmbeddingAdapter
-from .plugins.metadata_adapter import MetadataAdapter
 
-__all__ = [
-    "WrapperPipeline",
-    "CodeChunkAdapter",
-    "DocumentChunkAdapter",
-    "VideoChunkAdapter",
-    "EmbeddingAdapter",
-    "MetadataAdapter",
-]
+__all__ = ["WrapperPipeline"]
